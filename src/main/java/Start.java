86ffb89956492e8d8f7113166bb92dd4602491a8
@Koprov <solenit@mail.ru>
2022-03-05 13:28:20 +0300

    more update

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,3 +1,5 @@
+import data.labirinth.Map;
+
 public class Start {
     public static void main(String[] args){
         System.out.println("hello labirinth");
--- src/main/java/data/labirinth/generate/Point.java
+++ src/main/java/data/labirinth/generate/Point.java
@@ -3,8 +3,10 @@ package data.labirinth.generate;
 public class Point {
     private boolean isFinish;
     private boolean isHero;
+    private char isBlock;
     public Point(){
-        isFinish = false;
-        isHero = false;
+        this.isFinish = false;
+        this.isHero = false;
+        this.isBlock = PointGraphics.BLOCK.getValue();
     }
 }
